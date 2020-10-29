ca7052a926c953db07f6cbef5ca9c7731511da90
Haloren <mckennakyler@gmail.com>
2020-10-29 15:11:53 -0700

    cleaning up html white space and footer css

---
+++ frontend/src/ItemForm.js
@@ -0,0 +1,12 @@
+class ItemForm {
+    constructor(){
+
+    }
+
+
+}
+
+{/* <form id="add-item" class="add-item">
+        <input type="text" name="name" placeholder="Add Item">
+        <input type="submit" id="add-btn" class="add-btn" value="+">
+</form> */}
